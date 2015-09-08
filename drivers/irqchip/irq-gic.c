820555eca180b469fe49e499f89f468366981fae
Geert Uytterhoeven <geert+renesas@glider.be>
2015-09-08 14:11:46 +0200

    Revert "irqchip/gic: Enable SKIP_SET_WAKE and MASK_ON_SUSPEND"
    This reverts commit aec89ef72ba6c94420f599dcb684ed66937cdacf.

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -324,9 +324,7 @@ static struct irq_chip gic_chip = {
 #endif
 	.irq_get_irqchip_state	= gic_irq_get_irqchip_state,
 	.irq_set_irqchip_state	= gic_irq_set_irqchip_state,
-	.flags			= IRQCHIP_SET_TYPE_MASKED |
-				  IRQCHIP_SKIP_SET_WAKE |
-				  IRQCHIP_MASK_ON_SUSPEND,
+	.flags			= IRQCHIP_SET_TYPE_MASKED,
 };
 
 void __init gic_cascade_irq(unsigned int gic_nr, unsigned int irq)
